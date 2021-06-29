498cc8a39ec7b5e07caa8ffa7fb48fa42e21962c
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-06-29 13:38:41 +0200

    Fix #50 - Added uhtml/json export

---
+++ cjs/json.js
@@ -0,0 +1,51 @@
+'use strict';
+const umap = (m => /* c8 ignore start */ m.__esModule ? m.default : m /* c8 ignore stop */)(require('umap'));
+const {render: $render, html, svg} = require('./index.js');
+
+// Sender (SW, Worker, postMessage)
+const ids = umap(new WeakMap);
+let id = 0;
+
+const tag = type => (template, ...values) => ({
+  type,
+  template,
+  values,
+  id: ids.get(template) ||
+      ids.set(template, id++)
+});
+
+html.json = tag('html');
+svg.json = tag('svg');
+
+// Receiver (onmessage, from SW, Workers, etc)
+const templates = umap(new Map);
+
+const unroll = ({type, template, values, id}) => (
+  (type === 'svg' ? svg : html).apply(
+    null,
+    [
+      templates.get(id) ||
+      templates.set(id, template)
+    ].concat(values.map(asJSON))
+  )
+);
+
+const asJSON = value => isJSON(value) ? unroll(value) : value;
+
+const isJSON = thing => (
+  typeof thing === 'object' &&
+  thing !== null &&
+  'type' in thing &&
+  'template' in thing &&
+  'values' in thing &&
+  'id' in thing
+);
+
+const render = (where, what) => $render(
+  where,
+  isJSON(what) ? unroll(what) : what
+);
+
+exports.render = render;
+exports.html = html;
+exports.svg = svg;
---
+++ esm/json.js
@@ -0,0 +1,48 @@
+import umap from 'umap';
+import {render as $render, html, svg} from './index.js';
+
+// Sender (SW, Worker, postMessage)
+const ids = umap(new WeakMap);
+let id = 0;
+
+const tag = type => (template, ...values) => ({
+  type,
+  template,
+  values,
+  id: ids.get(template) ||
+      ids.set(template, id++)
+});
+
+html.json = tag('html');
+svg.json = tag('svg');
+
+// Receiver (onmessage, from SW, Workers, etc)
+const templates = umap(new Map);
+
+const unroll = ({type, template, values, id}) => (
+  (type === 'svg' ? svg : html).apply(
+    null,
+    [
+      templates.get(id) ||
+      templates.set(id, template)
+    ].concat(values.map(asJSON))
+  )
+);
+
+const asJSON = value => isJSON(value) ? unroll(value) : value;
+
+const isJSON = thing => (
+  typeof thing === 'object' &&
+  thing !== null &&
+  'type' in thing &&
+  'template' in thing &&
+  'values' in thing &&
+  'id' in thing
+);
+
+const render = (where, what) => $render(
+  where,
+  isJSON(what) ? unroll(what) : what
+);
+
+export {render, html, svg};
