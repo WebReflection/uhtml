7c1799c8b9d38d1a8b3adae2debb371611b869d5
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-12-09 15:07:42 +0100

    updated devDependencies

--- index.js
+++ index.js
@@ -150,98 +150,98 @@ self.uhtml = (function (exports) {
         }
       } // remove head or tail: fast path
       else if (bEnd === bStart) {
-          while (aStart < aEnd) {
-            // remove the node only if it's unknown or not live
-            if (!map || !map.has(a[aStart])) parentNode.removeChild(get(a[aStart], -1));
-            aStart++;
+        while (aStart < aEnd) {
+          // remove the node only if it's unknown or not live
+          if (!map || !map.has(a[aStart])) parentNode.removeChild(get(a[aStart], -1));
+          aStart++;
+        }
+      } // same node: fast path
+      else if (a[aStart] === b[bStart]) {
+        aStart++;
+        bStart++;
+      } // same tail: fast path
+      else if (a[aEnd - 1] === b[bEnd - 1]) {
+        aEnd--;
+        bEnd--;
+      } // The once here single last swap "fast path" has been removed in v1.1.0
+      // https://github.com/WebReflection/udomdiff/blob/single-final-swap/esm/index.js#L69-L85
+      // reverse swap: also fast path
+      else if (a[aStart] === b[bEnd - 1] && b[bStart] === a[aEnd - 1]) {
+        // this is a "shrink" operation that could happen in these cases:
+        // [1, 2, 3, 4, 5]
+        // [1, 4, 3, 2, 5]
+        // or asymmetric too
+        // [1, 2, 3, 4, 5]
+        // [1, 2, 3, 5, 6, 4]
+        var _node = get(a[--aEnd], -1).nextSibling;
+        parentNode.insertBefore(get(b[bStart++], 1), get(a[aStart++], -1).nextSibling);
+        parentNode.insertBefore(get(b[--bEnd], 1), _node); // mark the future index as identical (yeah, it's dirty, but cheap 👍)
+        // The main reason to do this, is that when a[aEnd] will be reached,
+        // the loop will likely be on the fast path, as identical to b[bEnd].
+        // In the best case scenario, the next loop will skip the tail,
+        // but in the worst one, this node will be considered as already
+        // processed, bailing out pretty quickly from the map index check
+
+        a[aEnd] = b[bEnd];
+      } // map based fallback, "slow" path
+      else {
+        // the map requires an O(bEnd - bStart) operation once
+        // to store all future nodes indexes for later purposes.
+        // In the worst case scenario, this is a full O(N) cost,
+        // and such scenario happens at least when all nodes are different,
+        // but also if both first and last items of the lists are different
+        if (!map) {
+          map = new Map();
+          var i = bStart;
+
+          while (i < bEnd) {
+            map.set(b[i], i++);
           }
-        } // same node: fast path
-        else if (a[aStart] === b[bStart]) {
-            aStart++;
-            bStart++;
-          } // same tail: fast path
-          else if (a[aEnd - 1] === b[bEnd - 1]) {
-              aEnd--;
-              bEnd--;
-            } // The once here single last swap "fast path" has been removed in v1.1.0
-            // https://github.com/WebReflection/udomdiff/blob/single-final-swap/esm/index.js#L69-L85
-            // reverse swap: also fast path
-            else if (a[aStart] === b[bEnd - 1] && b[bStart] === a[aEnd - 1]) {
-                // this is a "shrink" operation that could happen in these cases:
-                // [1, 2, 3, 4, 5]
-                // [1, 4, 3, 2, 5]
-                // or asymmetric too
-                // [1, 2, 3, 4, 5]
-                // [1, 2, 3, 5, 6, 4]
-                var _node = get(a[--aEnd], -1).nextSibling;
-                parentNode.insertBefore(get(b[bStart++], 1), get(a[aStart++], -1).nextSibling);
-                parentNode.insertBefore(get(b[--bEnd], 1), _node); // mark the future index as identical (yeah, it's dirty, but cheap 👍)
-                // The main reason to do this, is that when a[aEnd] will be reached,
-                // the loop will likely be on the fast path, as identical to b[bEnd].
-                // In the best case scenario, the next loop will skip the tail,
-                // but in the worst one, this node will be considered as already
-                // processed, bailing out pretty quickly from the map index check
-
-                a[aEnd] = b[bEnd];
-              } // map based fallback, "slow" path
-              else {
-                  // the map requires an O(bEnd - bStart) operation once
-                  // to store all future nodes indexes for later purposes.
-                  // In the worst case scenario, this is a full O(N) cost,
-                  // and such scenario happens at least when all nodes are different,
-                  // but also if both first and last items of the lists are different
-                  if (!map) {
-                    map = new Map();
-                    var i = bStart;
-
-                    while (i < bEnd) {
-                      map.set(b[i], i++);
-                    }
-                  } // if it's a future node, hence it needs some handling
+        } // if it's a future node, hence it needs some handling
 
 
-                  if (map.has(a[aStart])) {
-                    // grab the index of such node, 'cause it might have been processed
-                    var index = map.get(a[aStart]); // if it's not already processed, look on demand for the next LCS
+        if (map.has(a[aStart])) {
+          // grab the index of such node, 'cause it might have been processed
+          var index = map.get(a[aStart]); // if it's not already processed, look on demand for the next LCS
 
-                    if (bStart < index && index < bEnd) {
-                      var _i = aStart; // counts the amount of nodes that are the same in the future
+          if (bStart < index && index < bEnd) {
+            var _i = aStart; // counts the amount of nodes that are the same in the future
 
-                      var sequence = 1;
+            var sequence = 1;
 
-                      while (++_i < aEnd && _i < bEnd && map.get(a[_i]) === index + sequence) {
-                        sequence++;
-                      } // effort decision here: if the sequence is longer than replaces
-                      // needed to reach such sequence, which would brings again this loop
-                      // to the fast path, prepend the difference before a sequence,
-                      // and move only the future list index forward, so that aStart
-                      // and bStart will be aligned again, hence on the fast path.
-                      // An example considering aStart and bStart are both 0:
-                      // a: [1, 2, 3, 4]
-                      // b: [7, 1, 2, 3, 6]
-                      // this would place 7 before 1 and, from that time on, 1, 2, and 3
-                      // will be processed at zero cost
+            while (++_i < aEnd && _i < bEnd && map.get(a[_i]) === index + sequence) {
+              sequence++;
+            } // effort decision here: if the sequence is longer than replaces
+            // needed to reach such sequence, which would brings again this loop
+            // to the fast path, prepend the difference before a sequence,
+            // and move only the future list index forward, so that aStart
+            // and bStart will be aligned again, hence on the fast path.
+            // An example considering aStart and bStart are both 0:
+            // a: [1, 2, 3, 4]
+            // b: [7, 1, 2, 3, 6]
+            // this would place 7 before 1 and, from that time on, 1, 2, and 3
+            // will be processed at zero cost
 
 
-                      if (sequence > index - bStart) {
-                        var _node2 = get(a[aStart], 0);
+            if (sequence > index - bStart) {
+              var _node2 = get(a[aStart], 0);
 
-                        while (bStart < index) {
-                          parentNode.insertBefore(get(b[bStart++], 1), _node2);
-                        }
-                      } // if the effort wasn't good enough, fallback to a replace,
-                      // moving both source and target indexes forward, hoping that some
-                      // similar node will be found later on, to go back to the fast path
-                      else {
-                          parentNode.replaceChild(get(b[bStart++], 1), get(a[aStart++], -1));
-                        }
-                    } // otherwise move the source forward, 'cause there's nothing to do
-                    else aStart++;
-                  } // this node has no meaning in the future list, so it's more than safe
-                  // to remove it, and check the next live node out instead, meaning
-                  // that only the live list index should be forwarded
-                  else parentNode.removeChild(get(a[aStart++], -1));
-                }
+              while (bStart < index) {
+                parentNode.insertBefore(get(b[bStart++], 1), _node2);
+              }
+            } // if the effort wasn't good enough, fallback to a replace,
+            // moving both source and target indexes forward, hoping that some
+            // similar node will be found later on, to go back to the fast path
+            else {
+              parentNode.replaceChild(get(b[bStart++], 1), get(a[aStart++], -1));
+            }
+          } // otherwise move the source forward, 'cause there's nothing to do
+          else aStart++;
+        } // this node has no meaning in the future list, so it's more than safe
+        // to remove it, and check the next live node out instead, meaning
+        // that only the live list index should be forwarded
+        else parentNode.removeChild(get(a[aStart++], -1));
+      }
     }
 
     return b;
@@ -403,7 +403,7 @@ self.uhtml = (function (exports) {
 
             if (newValue.length === 0) nodes = diff(comment, nodes, []); // or diffed, if these contains nodes or "wires"
             else if (typeof(newValue[0]) === 'object') nodes = diff(comment, nodes, newValue); // in all other cases the content is stringified as is
-              else anyContent(String(newValue));
+            else anyContent(String(newValue));
             break;
           } // if the new value is a DOM node, or a wire, and it's
           // different from the one already live, then it's diffed.
@@ -757,11 +757,11 @@ self.uhtml = (function (exports) {
       if (hole instanceof Hole) values[i] = unroll(stack[i] || (stack[i] = createCache()), hole); // arrays are recursively resolved so that each entry will contain
       // also a DOM node or a wire, hence it can be diffed if/when needed
       else if (isArray(hole)) unrollValues(stack[i] || (stack[i] = createCache()), hole, hole.length); // if the value is nothing special, the stack doesn't need to retain data
-        // this is useful also to cleanup previously retained data, if the value
-        // was a Hole, or an Array, but not anymore, i.e.:
-        // const update = content => html`<div>${content}</div>`;
-        // update(listOfItems); update(null); update(html`hole`)
-        else stack[i] = null;
+      // this is useful also to cleanup previously retained data, if the value
+      // was a Hole, or an Array, but not anymore, i.e.:
+      // const update = content => html`<div>${content}</div>`;
+      // update(listOfItems); update(null); update(html`hole`)
+      else stack[i] = null;
     }
 
     if (length < stack.length) stack.splice(length);
@@ -877,4 +877,4 @@ self.uhtml = (function (exports) {
 
   return exports;
 
-}({}));
+})({});
