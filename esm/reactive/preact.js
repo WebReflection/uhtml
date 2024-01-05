096b0433d7e812b6ec234a6c7ee48fbb89d2bff1
webreflection <andrea.giammarchi@gmail.com>
2024-01-05 09:57:09 +0100

    Reordered import/export

--- esm/reactive/preact.js
+++ esm/reactive/preact.js
@@ -1,6 +1,6 @@
-import { effect } from '@preact/signals-core';
 export * from '@preact/signals-core';
-
-import { reactive } from '../reactive.js';
 export { Hole, html, svg, htmlFor, svgFor, attr } from '../reactive.js';
+
+import { effect } from '@preact/signals-core';
+import { reactive } from '../reactive.js';
 export const render = reactive(effect);
--- esm/reactive/signal.js
+++ esm/reactive/signal.js
@@ -1,6 +1,6 @@
-import { effect } from '@webreflection/signal';
 export * from '@webreflection/signal';
-
-import { reactive } from '../reactive.js';
 export { Hole, html, svg, htmlFor, svgFor, attr } from '../reactive.js';
+
+import { effect } from '@webreflection/signal';
+import { reactive } from '../reactive.js';
 export const render = reactive(effect);
